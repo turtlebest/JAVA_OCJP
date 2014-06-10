7ec197f4e0c1c38df19e2bc6e7d69e5bcc6e9736
trutlebest <d2118370@gmail.com>
2014-06-10 23:27:39 +0800

    11

--- Pratice/src/Print.java
+++ Pratice/src/Print.java
@@ -1,9 +1,9 @@
   
+import finance.Stock;
+
 public class Print {
   public static void main(String[] args) {
     Greeting a = new Greeting();
     a.greet();
-    System.out.print(args[0]);
-    System.out.print(args[1]);
-    } 
+  } 
 }
